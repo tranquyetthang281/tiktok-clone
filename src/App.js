82e94cc0f27450ce4ba43a974e9b413d61ed5d6a
tranquyetthang281 <79098631+tranquyetthang281@users.noreply.github.com>
2024-01-04 16:53:10 +0700

    build default layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import { useState } from "react";
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from './routes';
+import { DefaultLayout } from './components/Layout';
 
 function App() {
   return (
-    <div className="App">
-      <h1>React App</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {
+            publicRoutes.map((route, index) => {
+              const Page = route.component;
+
+              let Layout = DefaultLayout;
+              if (route.layout) {
+                Layout = route.Layout;
+              }
+              else if (route.Layout === null) {
+                Layout = Fragment
+              }
+
+              return <Route
+                key={index}
+                path={route.path}
+                element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                } />
+            })
+          }
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
